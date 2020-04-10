e83ea35395bbebe5b51e0a0c756150140cc0b780
Swati N Murthy <swati.snm20@gmail.com>
2020-04-10 16:50:11 +0530

    var, let, const

---
+++ index.js
@@ -0,0 +1,22 @@
+// var - Function scope
+// let - Block scope
+// const - Block scope, but read only
+
+function sayHello() {
+    for (var i=0; i<5;i++) {
+        console.log(i);
+    }
+    console.log(i);
+
+    for (let j=0; j<5; j++) {
+        console.log(j);
+    }
+    console.log(j); //Throws error
+
+    const x=5;
+    x=6; //Error
+}
+
+sayHello();
+
+
